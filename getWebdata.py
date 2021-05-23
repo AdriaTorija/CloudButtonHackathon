d637853ece42cb7566537b25f2fe9fbc4330a31e
Adrià Rubio <adria.rubio@estudiants.urv.cat>
2021-05-23 18:39:46 +0200

    Molts canvis

--- getWebdata.py
+++ getWebdata.py
@@ -1,35 +1,119 @@
 import scrapy
+from scrapy import linkextractors
 from scrapy.crawler import CrawlerProcess
 from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule, CrawlSpider
+#from DataBlogger import DatabloggerScraperItem
 import json
+import pandas as pd
 from lithops import Storage
 from analyseHtml import getText
+import os
+
 bucket='cloudbuttonhackathon'                  #Change this value if you want to change the storage bucket
+words= ['Covid', 'covid19', 'virus', 'Coronavirus', 'COVID-19', 'COVID', 'vaccine', 'Vaccine', 'positive', 'Positive', 'Hospital', 'hospital']
+names=[]
+linksss=[]
+items = []
+
+filenames=[]
+texts=[]
+votes=[]
 
 class TestSpider(scrapy.Spider):
   name = "test" 
 
+  allowed_domains = ['reddit.com'  ]
+
+  # The URLs to start with
+  start_urls = ['https://www.reddit.com/r/COVID19/','https://www.reddit.com/r/COVID19positive/','https://www.reddit.com/r/Coronavirus/']  
+  # This spider has one rule: extract all (unique and canonicalized) links, follow them and parse them using the parse_items method
+  rules = [
+      Rule(
+          LinkExtractor(
+              canonicalize=True,
+              unique=True
+          ),
+          follow=True,
+          callback="parse_item"
+      )
+  ]
+
+  maxdepth = 2
+
+  def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+
+  """ def parse_item(self, response):
+        # The list of items that are found on the particular page
+        
+        # Only extract canonicalized and unique links (with respect to the current page)
+        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)
+        # Now go through all the found links
+        for link in links:
+            # Check whether the domain of the URL of the link is allowed; so whether it is in one of the allowed domains
+            is_allowed = False
+            for allowed_domain in self.allowed_domains:
+                if allowed_domain in link.url:
+                    is_allowed = True
+            # If it is allowed, create a new item and add it to the list of found items
+            if is_allowed:
+                item = DatabloggerScraperItem()
+                item['url_from'] = response.url
+                item['url_to'] = link.url
+                items.append(item)
+                linksss.append(link.url)
+        for link_to_follow in items:
+            yield scrapy.request(link_to_follow['url_from'], callback=self.parse)
+                
+        # Return all the found items
+        #return items """
 
   def parse(self, response):
-    urls_all = []    # Used to collect all the links first.
-    urls_follow = [] # Links to finally follow/crawl
-    # Find and extract all links into the list.
-    for link in response.css('a::attr(href)').extract():
-      url = response.urljoin(link)
-      urls_all.append(url)
+      # Set default meta information for first page
+      from_url = ''
+      from_text = ''
+      depth = 0;
+      # Extract the meta information from the response, if any
+      if 'from' in response.meta:
+          from_url = response.meta['from']
+      if 'text' in response.meta:
+          from_text = response.meta['text']
+      if 'depth' in response.meta:
+          depth = response.meta['depth']
+      titles = response.css('._eYtD2XCVieq6emjKBH3m::text').extract()       
+      vot = response.css('._1rZYMD_4xY3gRcSS3p8ODO::text').extract()
+      comments = response.css('.FHCV02u6Cp2zYL0fhQPsO::text').extract()
+      linksss.append(response.url)
+      filenames.append(titles)
+      texts.append(comments)
+      votes.append(vot)
+      # Update the print logic to show what page contain a link to the
+      # current page, and what was the text of the link
+      print(depth, response.url, '<-', from_url, from_text, sep=' ')
+      # Browse a tags only if maximum depth has not be reached
+      if depth < self.maxdepth:
+          a_selectors = response.xpath("//a")
+          for selector in a_selectors:
+              
+              text = selector.xpath("text()").extract_first()
+              link = selector.xpath("@href").extract_first()
+              request = response.follow(link, callback=self.parse)
+              # Meta information: URL of the current page
+              request.meta['from'] = response.url
+              
+              # Meta information: text of the link
+              request.meta['text'] = text
+              
+              # Meta information: depth of the link
+              request.meta['depth'] = depth + 1
+              
+              """ storage.put_object(bucket,link,text) """
 
-    # Analyze and remove unwanted links
-    # Assuming we wish to ignore/drop urls odd-number indexed links
-    # Add only even number indexed links to urls_follow 
-    for i in range( len( urls_all ) ):
-      if i % 2 == 0:
-        urls_follow.append( urls_all[i] )
-
-    # Finally let's create and yield all the requests to even numbered index links
-    for link_to_follow in urls_follow:
-      yield scrapy.Request(link_to_follow, callback=self.processEvenNumberedLink)
+              yield request
       
-  def processEvenNumberedLink(self, response):
+  """ def parse(self, response):
     # Do something useful/meaningful.
     self.logger.info("Following %s", response.url) 
     page = response.url.split("/")[-2]
@@ -38,33 +122,61 @@ class TestSpider(scrapy.Spider):
     #   f.write(response.body)
     storage = Storage()
     print(filename)
-    storage.put_object(bucket,filename,getText(response.body))
+    storage.put_object(bucket,filename,getText(response.body)) """
 
 process = CrawlerProcess()
 
-
-def getWebsHtml(link):
-    #TestSpider.start_urls=['https://www.reddit.com/r/COVID19/']     #Example
-    TestSpider.start_urls=[link]
+if __name__ == '__main__':
+    process = CrawlerProcess()
     process.crawl(TestSpider)
-    process.start() # the script will block here until the crawling is finished
-    storage = Storage()
+    process.start()
+
+    storage=Storage()
     list=storage.list_keys(bucket)
-    filenames=[]
-    texts=[]
-    for a in list:
-      filenames.append(a)
-      text=storage.get_object(bucket,a)
-      texts.append(str(text))
+    
+
+    """ for a in list:
+        num=[]
+        name=a.split('.')[0]
+        if name != 'lithops' and name != 'dataWEB':
+          filenames.append(a)
+          text=storage.get_object(bucket,a)
+          texts.append(str(text))
+          texto=str(text).split()
+          i=0
+          for b in words:
+              y=0
+              num.append(y)
+              for c in texto:
+                  if b == c:
+                    y=y+1
+                    num[i]=y
+              i=i+1
+          names.append(num) """
+      
     dict={
-      "filenames":filenames,
-      "texts":texts,
+        "filenames":linksss,
+        "Titulos":filenames, 
+        "comments":texts,
+        "votes": votes
         }
 
-    list=storage.list_keys('cloudbuttonhackathon')
+    list=storage.list_keys(bucket)
     storage.delete_objects('cloudbuttonhackathon',list)
 
     storage.put_object(bucket,"dataWEB.json",json.dumps(dict))
+
+    pdOBJ = pd.read_json(storage.get_object(bucket, "dataWEB.json"), orient='index')
+    pdOBJ.to_csv('data.csv', index=False)
+""" def getWebsHtml():
+    #TestSpider.start_urls=['https://www.reddit.com/r/COVID19/']     #Example
+    process = CrawlerProcess()
+    process.crawl(TestSpider)
+    process.start() # the script will block here until the crawling is finished """
+
+    
+
+      
 
 
 
--- test.py
+++ test.py
@@ -1,12 +1,23 @@
+import json, os
 import lithops
+import pandas as pd
+from lithops import Storage
 fexec = lithops.ServerlessExecutor()
 from getWebdata import getWebsHtml
 from lithops.multiprocessing import Pool
 
-webs=['https://www.reddit.com/r/COVID19/','https://www.reddit.com/r/COVID19positive/','https://www.reddit.com/r/Coronavirus/']
-    
-for i in webs:
-    fexec.call_async(getWebsHtml,i) 
+webs=['https://www.reddit.com/r/COVID19/']
+web=['https://www.reddit.com/r/COVID19/','https://www.reddit.com/r/COVID19positive/','https://www.reddit.com/r/Coronavirus/']  
 
-from lithops import Storage
+bucket='cloudbuttonhackathon'
+storage=Storage()
+#for i in storage.list_keys(bucket):
+#    storage.delete_object(bucket,i)
+data=[]
+#for i in web:
+fexec.call_async(getWebsHtml, data)
+
+
+pdOBJ = pd.read_json(storage.get_object(bucket, "dataWEB.json"), orient='index')
+pdOBJ.to_csv('data.csv', index=False)
 
