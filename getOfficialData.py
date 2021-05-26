7253ab63cac1a2913e70eaefdfc7eaf258a6d638
Adria Torija <adria.torija@estudiants.urv.cat>
2021-05-26 16:10:26 +0200

    DataAnalysing

--- getOfficialData.py
+++ getOfficialData.py
@@ -9,7 +9,6 @@ bucket='cloudbuttonhackathon'                  #Change this value if you want to
 
 def dataSearch(keysText,hashtag,number_of_tweets):                       #keys: String of the file with keys
     #Reading the keys for connection                #Hashtag: hashtag we want to search and save tweets without #
-    print("Keys:\n"+keysText)
     keys= open(keysText,'r').read().splitlines()
     consumer_key=keys[0]
     consumer_secret=keys[1]
@@ -40,12 +39,11 @@ def dataSearch(keysText,hashtag,number_of_tweets):                       #keys: 
 
     stringSearch="#"+hashtag + lang 
     print("Searching: "+stringSearch)
-    #for i in tweepy.Cursor(api.search,q=stringSearch,tweet_mode="extended",until="2021-05-22").items(number_of_tweets):
-    for i in tweepy.Cursor(api.search_30_day("CloudButton0",q=stringSearch,fromDate="2021-05-0",toDate="2021-05-12",maxResults=4)).items(number_of_tweets):
+    for i in tweepy.Cursor(api.search,q=stringSearch,tweet_mode="extended",until="2021-05-22").items(number_of_tweets):
+    #for i in tweepy.Cursor(api.search_30_day("CloudButton0",q=stringSearch,fromDate="2021-05-0",toDate="2021-05-12",maxResults=4)).items(number_of_tweets):
         tweets.append(i.full_text)
         likes.append(i.favorite_count)
         time.append(str(i.created_at))
-        print(i.created_at)
         for j in i.entities["hashtags"]:
             hashtags.append(j["text"])
         urls.append(f"https://twitter.com/user/status/{i.id}")
@@ -72,15 +70,14 @@ def dataSearch(keysText,hashtag,number_of_tweets):                       #keys: 
     }
     with open("proves.json", 'w') as f:
        f.write(json.dumps(dict))
-    print(dict)
+    
     #storage.put_object(bucket,"dataTwitter.json",json.dumps(dict))
     #storage.put_object(bucket,"text.txt",str(tweets))
     
 
 
 def main():
-    dataSearch("keys.txt","Covid19",4)
-    print("gg")
+    dataSearch("keys.txt","Covid19",250)
 
 if __name__ == "__main__":
     main()
