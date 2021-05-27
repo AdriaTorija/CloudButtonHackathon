8ce5e518b3eed6b50f09f812b48e2b1f4e03144d
Adria Torija <adria.torija@estudiants.urv.cat>
2021-05-27 17:38:40 +0200

    Preprocessing

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -6,14 +6,18 @@ import json
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from lithops import Storage
 from io import BytesIO
-
-
+from collections import Counter
 bucket='cloudbuttonhackathon'
+def mostCommonWords(df,n):
+    splitText = []
+    print(df)
+    for text in df["Text"]:
+        for word in str(text).split():
+            splitText.append(word)
+        m = Counter(splitText).most_common(n)
+    return m
 
-def analysis():
-    storage=Storage()
-    data=storage.get_object(bucket,"prova.csv")
-    df = pd.read_csv(BytesIO(data))
+def feelings(df):
     analyzer = SentimentIntensityAnalyzer()
     pos, neg, neu = 0, 0, 0
     positive, neutral, negative = [], [], []
@@ -32,10 +36,46 @@ def analysis():
             neg += 1
             if (text) not in negative:
                 negative.append(text)
-    print(pos)
-    print(neg)
-    print(neu)
+  
+    dict={
+        "Positives":positive,
+        "Neutrals":neutral,
+        "Negatives":negative,        
+    }
+    return dict
 
+def mostCommonHashtags(df,n):
+    splitHash=[]
+    for i in df["Hashtags"]:
+        for word in str(i).split(','):
+            if(word!='' and word!='nan'):
+                splitHash.append(word.upper())
+        m = Counter(splitHash).most_common(n)
+    return m
+
+def mostRetweeted(df,n):
+    return max(df["Retweets"])
+
+def verifiedTweet(df):
+    i=0
+    x=[]
+    for j in df["Verified"]:
+        
+        if not j:
+            x.append(df.loc[[i]])
+        i = i+1
+    return x
+            
+def analysis():
+    storage=Storage()
+    data=storage.get_object(bucket,"prova.csv")
+    df = pd.read_csv(BytesIO(data))
+    
+    
+   
+    
+
+    
  
 def main():
     analysis()
