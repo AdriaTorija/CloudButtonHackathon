e6db351d1ebcfae416f33803186eac5737c725b9
Adria Torija <adria.torija@estudiants.urv.cat>
2021-05-27 15:06:12 +0200

    Analysing Text update to csv

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -4,34 +4,39 @@ import getOfficialData as getOData
 import nltk
 import json
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from lithops import Storage
+from io import BytesIO
 
+
+bucket='cloudbuttonhackathon'
 
 def analysis():
-    with open("proves.json", 'r') as f:
-        data= json.load(f)
-        analyzer = SentimentIntensityAnalyzer()
-        pos, neg, neu = 0, 0, 0
-        positive, neutral, negative = [], [], []
-        for text in (data["tweets"]):
-            vs = analyzer.polarity_scores(str(text))
-        #polary = analyzer.polarity_scores()
-            if vs['compound'] >= 0.4:
-                pos += 1
-                if (text) not in positive:
-                    positive.append(text)
-            elif vs['compound'] < 0.4 and vs['compound'] > -0.4:
-                neu += 1
-                if (text) not in neutral:
-                    neutral.append((text))
-            else:
-                neg += 1
-                if (text) not in negative:
-                    negative.append(text)
-        print(pos)
-        print(neg)
-        print(neu)
+    storage=Storage()
+    data=storage.get_object(bucket,"prova.csv")
+    df = pd.read_csv(BytesIO(data))
+    analyzer = SentimentIntensityAnalyzer()
+    pos, neg, neu = 0, 0, 0
+    positive, neutral, negative = [], [], []
+    for text in (df["Text"]):
+        vs = analyzer.polarity_scores(str(text))
+    #polary = analyzer.polarity_scores()
+        if vs['compound'] >= 0.4:
+            pos += 1
+            if (text) not in positive:
+                positive.append(text)
+        elif vs['compound'] < 0.4 and vs['compound'] > -0.4:
+            neu += 1
+            if (text) not in neutral:
+                neutral.append((text))
+        else:
+            neg += 1
+            if (text) not in negative:
+                negative.append(text)
+    print(pos)
+    print(neg)
+    print(neu)
 
-
+ 
 def main():
     analysis()
     print("End of analysis")
